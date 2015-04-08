e13dd22273eb74bef02fc30cd696a1263de4a83a
HeldyHeileng <heileng@mail.ru>
2015-04-08 16:09:46 +0300

    TestformDesign

--- NewCompany/TestForm.Designer.cs
+++ NewCompany/TestForm.Designer.cs
@@ -28,22 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.ContentLabel = new System.Windows.Forms.Label();
+            this.NextStepButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NameLabel.Location = new System.Drawing.Point(50, 25);
+            this.NameLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(75, 25);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Name";
+            // 
+            // ContentLabel
+            // 
+            this.ContentLabel.AutoSize = true;
+            this.ContentLabel.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ContentLabel.Location = new System.Drawing.Point(50, 75);
+            this.ContentLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.ContentLabel.Name = "ContentLabel";
+            this.ContentLabel.Size = new System.Drawing.Size(97, 24);
+            this.ContentLabel.TabIndex = 1;
+            this.ContentLabel.Text = "Content";
+            // 
+            // NextStepButton
+            // 
+            this.NextStepButton.Location = new System.Drawing.Point(50, 150);
+            this.NextStepButton.Name = "NextStepButton";
+            this.NextStepButton.Size = new System.Drawing.Size(200, 50);
+            this.NextStepButton.TabIndex = 2;
+            this.NextStepButton.Text = "Следующая ситуация";
+            this.NextStepButton.UseVisualStyleBackColor = true;
             // 
             // TestForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(884, 661);
             this.ControlBox = false;
+            this.Controls.Add(this.NextStepButton);
+            this.Controls.Add(this.ContentLabel);
+            this.Controls.Add(this.NameLabel);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "TestForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "TestForm";
+            this.Text = "Тестирование началось...";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label ContentLabel;
+        private System.Windows.Forms.Button NextStepButton;
     }
 }
