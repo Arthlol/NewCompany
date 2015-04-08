25fd237af67414ed9f3e922bc3bdef8f7b72a4b5
HeldyHeileng <heileng@mail.ru>
2015-04-09 00:24:32 +0300

    dropdown

--- NewCompany/AnalyseForm.Designer.cs
+++ NewCompany/AnalyseForm.Designer.cs
@@ -28,7 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.AnswerTextBox = new System.Windows.Forms.TextBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // AnswerTextBox
+            // 
+            this.AnswerTextBox.Location = new System.Drawing.Point(340, 12);
+            this.AnswerTextBox.Multiline = true;
+            this.AnswerTextBox.Name = "AnswerTextBox";
+            this.AnswerTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.AnswerTextBox.Size = new System.Drawing.Size(532, 214);
+            this.AnswerTextBox.TabIndex = 0;
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(12, 12);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(269, 21);
+            this.comboBox.TabIndex = 1;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
             // 
             // Оценка
             // 
@@ -37,13 +57,20 @@
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(884, 662);
             this.ControlBox = false;
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.AnswerTextBox);
             this.Name = "Оценка";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AnalyseForm";
+            this.Load += new System.EventHandler(this.Оценка_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox AnswerTextBox;
+        private System.Windows.Forms.ComboBox comboBox;
     }
 }
--- NewCompany/AnalyseForm.cs
+++ NewCompany/AnalyseForm.cs
@@ -16,5 +16,32 @@ namespace NewCompany
         {
             InitializeComponent();
         }
+        Entities db = new Entities();
+        private void Оценка_Load(object sender, EventArgs e)
+        {
+            var AnswerList = db.Answer.ToList();
+            var SessionList = db.Session.ToList();
+            var SituaionList = db.Session.ToList();
+            comboBox.DataSource = db.Session.ToList();
+            comboBox.ValueMember =  "Id";
+            comboBox.DisplayMember = "Surname";
+            foreach (var element in AnswerList)
+            {
+                AnswerTextBox.Text += "Кому: " + element.Action + " " + "Что: " + element.PeopleGroup +"\r\n";
+            }
+        }
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            this.Text = comboBox.SelectedValue.ToString();
+#warning ошибка при конверте 
+            /*
+            var AnswerList = db.Answer.Where(x => x.SessionId == Convert.ToInt32(comboBox.SelectedValue)).ToList();
+            foreach (var element in AnswerList)
+            {
+                AnswerTextBox.Text += "Кому: " + element.Action + " " + "Что: " + element.PeopleGroup + "\r\n";
+            }*/
+        }
     }
 }
